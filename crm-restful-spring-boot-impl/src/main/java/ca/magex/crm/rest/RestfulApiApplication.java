aa7c6bbc00089a5b497d2c74251ddffe396e5373
magex9 <scott@magex.ca>
2020-03-24 11:27:38 -0400

    Initial import.

---
+++ crm-restful-spring-boot-impl/src/main/java/ca/magex/crm/rest/RestfulApiApplication.java
@@ -0,0 +1,23 @@
+package ca.magex.data.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ca.magex.crm.amnesia.services.OrganizationServiceAmnesiaImpl;
+import ca.magex.crm.amnesia.services.OrganizationServiceTestDataPopulator;
+import ca.magex.crm.api.services.OrganizationService;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+	@Bean
+	public OrganizationService organizations() {
+		return OrganizationServiceTestDataPopulator.populate(new OrganizationServiceAmnesiaImpl());
+	}
+
+}
