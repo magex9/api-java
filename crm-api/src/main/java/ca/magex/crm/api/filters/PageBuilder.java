5173fb696902aed84afb6df87b774f6af9782c69
Jonny <jonny.trafford@gmail.com>
2020-03-25 20:52:00 -0400

    missed the PageBuilder

---
+++ crm-api/src/main/java/ca/magex/crm/api/filters/PageBuilder.java
@@ -0,0 +1,23 @@
+package ca.magex.crm.api.filters;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+public class PageBuilder {
+	
+	public static <T> Page<T> buildPageFor(List<T> items, Pageable pageable) {
+		int fromIndex = (int) pageable.getOffset();
+		if (fromIndex > items.size()) {
+			return new PageImpl<T>(Collections.emptyList(), pageable, items.size());
+		}
+		int toIndex = fromIndex + pageable.getPageSize();
+		if (toIndex > items.size() - 1) {
+			toIndex = items.size();
+		}
+		return new PageImpl<T>(items.subList(fromIndex, toIndex), pageable, items.size());
+	}
+}
