d208125858c32948a5359a966c8eb6bc72e579c0
Jonny <jonny.trafford@gmail.com>
2020-04-01 08:43:19 -0400

    added Paging Comparator

--- crm-api/src/main/java/ca/magex/crm/api/filters/Paging.java
+++ crm-api/src/main/java/ca/magex/crm/api/filters/Paging.java
@@ -1,9 +1,14 @@
 package ca.magex.crm.api.filters;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Comparator;
+import java.util.Iterator;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.util.ReflectionUtils;
 
 public class Paging implements Pageable, Serializable {
 
@@ -77,4 +82,35 @@ public class Paging implements Pageable, Serializable {
 		return new Paging(getOffset() + getPageSize(), getPageSize(), getSort());
 	}
 	
+	/**
+	 * Creates a new Comparator based on this Paging Instance
+	 * @author Jonny
+	 *
+	 * @param <T>
+	 */
+	public class PagingComparator<T> implements Comparator<T> {
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compare(T o1, T o2) {
+			Iterator<Order> iterator = Paging.this.getSort().iterator();
+			while(iterator.hasNext()) {
+				Order order = iterator.next();
+				Field field = ReflectionUtils.findField(o1.getClass(), order.getProperty());
+				ReflectionUtils.makeAccessible(field);
+				Object val1 = ReflectionUtils.getField(field, o1);
+				Object val2 = ReflectionUtils.getField(field, o2);
+				if (Comparable.class.isAssignableFrom(val1.getClass())) {
+					Comparable<Object> cVal1 = (Comparable<Object>) val1;
+					Comparable<Object> cVal2 = (Comparable<Object>) val2;
+					int compare = cVal1.compareTo(cVal2);
+					if (compare !=0) {
+						return compare;
+					}
+				}
+			}
+			/* everything matched */
+			return 0;
+		}
+	}
 }
