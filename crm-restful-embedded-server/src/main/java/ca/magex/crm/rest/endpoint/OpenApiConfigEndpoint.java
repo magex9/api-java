751ecc78cafc01a755d132b3a9da14daddd097a5
magex9 <scott@magex.ca>
2020-04-01 07:55:21 -0400

    Added yaml and json config types

--- crm-restful-embedded-server/src/main/java/ca/magex/crm/rest/endpoint/OpenApiConfigEndpoint.java
+++ crm-restful-embedded-server/src/main/java/ca/magex/crm/rest/endpoint/OpenApiConfigEndpoint.java
@@ -1,10 +1,15 @@
 package ca.magex.crm.rest.endpoint;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -14,14 +19,14 @@ import ca.magex.crm.api.exceptions.PermissionDeniedException;
 import ca.magex.crm.ld.LinkedDataFormatter;
 import ca.magex.crm.ld.data.DataObject;
 import ca.magex.crm.ld.data.DataParser;
-import ca.magex.crm.rest.endpoint.Endpoint;
+import ca.magex.crm.rest.servlet.MimeType;
 
 public class OpenApiConfigEndpoint implements Endpoint {
 	
-	private DataObject config;
+	private Map<MimeType, String> configs;
 	
 	public OpenApiConfigEndpoint() {
-		this.config = buildConfig();
+		this.configs = new HashMap<MimeType, String>();
 	}
 	
 	public boolean isInterestedIn(HttpServletRequest req) {
@@ -29,20 +34,46 @@ public class OpenApiConfigEndpoint implements Endpoint {
 	}
 	
 	public String execute(HttpServletRequest req, HttpServletResponse res) throws ItemNotFoundException, PermissionDeniedException, IOException {
-		return config.stringify(LinkedDataFormatter.full());
+		String accepts = req.getHeader("Accept");
+		if (accepts == null) {
+			return getJsonConfig();
+		} else if (accepts.equals(MimeType.YAML.toString())) {
+			return getYamlConfig();
+		} else {
+			return getJsonConfig();
+		}
+	}
+		
+	private InputStream getConfigInputStream() {
+		return getClass().getClassLoader().getResourceAsStream("crm.yaml");
 	}
 	
-	protected DataObject buildConfig() {
-		try {
-			InputStream is = getClass().getClassLoader().getResourceAsStream("crm.yaml");
-			ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
-			Object obj = yamlReader.readValue(is, Object.class);
-			ObjectMapper jsonWriter = new ObjectMapper();
-			String json = jsonWriter.writeValueAsString(obj);
-			return (DataObject) DataParser.parse(json);
-		} catch (Exception e) {
-			throw new RuntimeException("Unable to parse config file", e);
+	private String getYamlConfig() {
+		if (!configs.containsKey(MimeType.YAML)) {
+			try {
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				IOUtils.copy(getConfigInputStream(), baos);
+				configs.put(MimeType.YAML, new String(baos.toByteArray()));
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to parse config file", e);
+			}
 		}
+		return configs.get(MimeType.YAML);
+	}
+	
+	private String getJsonConfig() {
+		if (!configs.containsKey(MimeType.JSON)) {
+			try {
+				ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+				Object obj = yamlReader.readValue(getConfigInputStream(), Object.class);
+				ObjectMapper jsonWriter = new ObjectMapper();
+				String json = jsonWriter.writeValueAsString(obj);
+				configs.put(MimeType.JSON, ((DataObject) DataParser.parse(json)).stringify(LinkedDataFormatter.full()));
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to parse config file", e);
+			}
+		}
+		return configs.get(MimeType.JSON);
 	}
 		
 }
