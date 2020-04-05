28dba080ae50da708cb9d67b2ddeaf0c375fc1b4
Jonny <jonny.trafford@gmail.com>
2020-04-05 19:39:14 -0400

    migrating data

---
+++ crm-graphql-jwt-spring-boot-server/src/main/java/ca/magex/crm/graphql/CrmGraphqlJwtApplication.java
@@ -0,0 +1,22 @@
+package ca.magex.crm.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ca.magex.crm.amnesia.services.AmnesiaFactory;
+import ca.magex.crm.amnesia.services.AmnesiaTestDataPopulator;
+import ca.magex.crm.api.services.SecuredCrmServices;
+
+@SpringBootApplication
+public class CrmGraphqlJwtApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CrmGraphqlJwtApplication.class, args);
+	}
+	
+	@Bean
+	public SecuredCrmServices organizations() {
+		return AmnesiaTestDataPopulator.populate(AmnesiaFactory.getAnonymousService());
+	}
+}
