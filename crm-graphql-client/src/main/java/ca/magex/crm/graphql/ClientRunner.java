26abebb6cf2aa16fbdbc430afc5035bc254c08d6
Jonny <jonny.trafford@gmail.com>
2020-04-22 16:14:55 -0400

    updated client runner

--- crm-graphql-client/src/main/java/ca/magex/crm/graphql/ClientRunner.java
+++ crm-graphql-client/src/main/java/ca/magex/crm/graphql/ClientRunner.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 import ca.magex.crm.api.crm.OrganizationDetails;
 import ca.magex.crm.graphql.client.CrmServicesGraphQLClientImpl;
 
+/**
+ * Used as a test script for the client
+ * 
+ * @author Jonny
+ */
 public class ClientRunner {
 
 	public static void main(String[] args) throws IOException {
 		try (CrmServicesGraphQLClientImpl crmImpl = new CrmServicesGraphQLClientImpl("http://localhost:9002/crm/graphql")) {
 			
-			crmImpl.authenticateJwt("http://localhost:9002/crm/authenticate", "CXA0", "admin");
-			
+			crmImpl.authenticateJwt("http://localhost:9002/crm/authenticate", "CXA0", "admin");			
 			OrganizationDetails org = crmImpl.createOrganization("Johnnuy");
 			System.out.println(org);
 		}
