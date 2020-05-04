96215f38fe29c1e85c77d3043676b7bdf0d37eaf
Jonny <jonny.trafford@gmail.com>
2020-05-04 09:50:43 -0400

    new zuul module

---
+++ crm-zuul-spring-boot-server/src/main/java/ca/magex/crm/crmzuulspringbootserver/CrmZuulSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package ca.magex.crm.crmzuulspringbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmZuulSpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmZuulSpringBootServerApplication.class, args);
+	}
+
+}
