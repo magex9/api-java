9cde373815263dc35fc110db106dfb6e0a98f5e6
eagleyes <eagleyes12@gmail.com>
2020-05-11 16:39:59 -0400

    Set up CI with Azure Pipelines
    [skip ci]
    
    Set up CI with Azure Pipelines
    
    [skip ci]
    
    Added installsshkey task
    removed duplicate installssh
    moved installSSHKey task
    added ssh to RD3
    
    Reorganized pipeline
    
    fixed typo
    Update azure-pipelines.yml for Azure Pipelines
    
    Changed master stage name
    Update azure-pipelines.yml for Azure Pipelines
    
    added maven and bash
    fixed task names in azure pipeline
    test
    split deployment in own stage
    put back environment in deploy
    added artifact publishing
    now copying files to RD3
    test
    test
    changed download folder
    Changed source folder in CopyOverSSH
    test
    added service.sh
    
    added .factorypath extension ignore
    
    fixed few things in service.sh
    
    now tarring package including admin folder and sending to server
    Update azure-pipelines.yml for Azure Pipelines
    fixed few bugs with pipeline
    fixed few bugs with pipeline
    fixed tar file inner file names
    added job and debugging code
    TYPO !!
    removed jobs since packaging and build must be in same job
    debugging tar command
    only targeting crm-auth-spring-boot-server
    trying few things
    typo (uppercase)
    changed tar content
    added configuration file
    added execute permission to service.sh

---
+++ crm-admin/admin/service.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+command=$1
+
+source $DIR/../config/application.cfg
+
+app_suffix="-bootable.jar"
+base_dir=/var/data/finuser
+JAVA_11_HOME=/usr/lib/jvm/jre-11-openjdk/
+JAVA_11_BIN=${JAVA_11_HOME}/bin/java
+logs_dir=${base_dir}/logs/${APPLICATION_NAME}
+work_dir=${base_dir}/work/${APPLICATION_NAME}
+app_base_dir=${DIR}/../
+pid_file=${work_dir}/${APPLICATION_NAME}.pid
+
+usage(){
+    echo ""
+    echo "Usage: $0 start|stop|restart|status application_name"
+    echo "Description: This service.sh command is to manage springboot applications bootable jars"
+    echo ""
+    exit 1
+}
+
+
+case $command in
+    "start" | "stop" | "restart" | "status" ) echo $command ;;
+    *) echo "command must be either start,stop,restart or status" ; usage ;;
+esac
+
+if [ -z $APPLICATION_NAME ] ;then
+    echo "You must supply a valid application name in configuration file "
+    usage
+fi
+
+jar_path="${work_dir}/${APPLICATION_NAME}-${APPLICATION_VERSION}${app_suffix}"
+
+ls -la $jar_path
+
+#check logs_dir
+if [ ! -d ${logs_dir} ];then
+    mkdir -p ${logs_dir}
+fi
+
+#check work_dir
+if [ ! -d ${work_dir} ];then
+    mkdir -p ${work_dir}
+fi
+
+start() {
+    #get status
+    status ; rtrn_cd=$?
+    if [ $rtrn_cd -eq 1 ];then 
+        #application running , must stop it first
+        echo "application already running"
+        exit 0
+    fi
+
+    rm -rf ${work_dir}/*
+    cp -ar ${app_base_dir}/* ${work_dir}/
+    nohup ${JAVA_11_BIN} -jar $jar_path > ${logs_dir}/${APPLICATION_NAME}.log &
+    pid=$!
+    echo $pid > ${pid_file}
+}
+
+stop() {
+    pid=cat ${pid_file}
+    if [ -z $pid ];then
+        echo "pid does not exists for application ${APPLICATION_NAME} assuming it is not running"
+    else
+        echo "killing pid $pid"
+        kill -15 $pid
+    fi
+}
+
+#status returns 
+# 0 : not running
+# 1 : running
+status(){
+    pid=`cat ${pid_file}`
+    if [ -z $pid ];then
+        echo "pid does not exists for application ${APPLICATION_NAME} assuming it is not running"
+        return 0
+    else
+        echo "Getting status of application $APPLICATION_NAME, pid $pid"
+        ps -fp $pid ; rtrn_cd=$?
+        if [ $rtrn_cd -eq 0 ];then
+            #application running
+            echo "application running"
+            return 1
+        else
+            #application not running
+            echo "application NOT running"
+            return 0
+        fi
+    fi
+
+}
+
+eval ${command}
