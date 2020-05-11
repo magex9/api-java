964c5a36aa7a622193f8ab939419c19143c36226
eagleyes <eagleyes12@gmail.com>
2020-05-11 16:04:30 -0400

    fixed few things in service.sh

--- crm-admin/admin/service.sh
+++ crm-admin/admin/service.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-command=$1
-app_name=$2
-app_suffix="-bootable.jar"
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+command=$1
+
+source $DIR/../config/application.cfg
+
+app_suffix="-bootable.jar"
+base_dir=/var/data/finuser
+JAVA_11_HOME=/usr/lib/jvm/jre-11-openjdk/
+JAVA_11_BIN=${JAVA_11_HOME}/bin/java
+logs_dir=${base_dir}/logs/${APPLICATION_NAME}
+work_dir=${base_dir}/work/${APPLICATION_NAME}
+app_base_dir=${DIR}/../
+pid_file=${work_dir}/${APPLICATION_NAME}.pid
 
 usage(){
     echo ""
@@ -18,43 +35,71 @@ case $command in
     *) echo "command must be either start,stop,restart or status" ; usage ;;
 esac
 
-#check application name
-ls $app_name > /dev/null
-app_exists=$?
-echo $app_exists
-if [ -z $app_name ] || [ ${app_exists} == 2 ];then
-    echo "You must supply a valid application name at argument 2"
+if [ -z $APPLICATION_NAME ] ;then
+    echo "You must supply a valid application name in configuration file "
     usage
 fi
 
-jar_path="$app_name/target/$app_name-*.*.*${app_suffix}"
+jar_path="${work_dir}/${APPLICATION_NAME}-${APPLICATION_VERSION}${app_suffix}"
 
 ls -la $jar_path
 
+#check logs_dir
+if [ ! -d ${logs_dir} ];then
+    mkdir -p ${logs_dir}
+fi
+
+#check work_dir
+if [ ! -d ${work_dir} ];then
+    mkdir -p ${work_dir}
+fi
 
 start() {
-    nohup java -jar $jar_path > logs/${app_name}.log &
+    #get status
+    status ; rtrn_cd=$?
+    if [ $rtrn_cd -eq 1 ];then 
+        #application running , must stop it first
+        echo "application already running"
+        exit 0
+    fi
+
+    rm -rf ${work_dir}/*
+    cp -ar ${app_base_dir}/* ${work_dir}/
+    nohup ${JAVA_11_BIN} -jar $jar_path > ${logs_dir}/${APPLICATION_NAME}.log &
     pid=$!
-    echo $pid > ${app_name}.pid
+    echo $pid > ${pid_file}
 }
 
 stop() {
-    pid=cat ${app_name}.pid
+    pid=cat ${pid_file}
     if [ -z $pid ];then
-        echo "pid does not exists for application ${app_name} assuming it is not running"
+        echo "pid does not exists for application ${APPLICATION_NAME} assuming it is not running"
     else
         echo "killing pid $pid"
         kill -15 $pid
     fi
 }
 
+#status returns 
+# 0 : not running
+# 1 : running
 status(){
-    pid=`cat ${app_name}.pid`
+    pid=`cat ${pid_file}`
     if [ -z $pid ];then
-        echo "pid does not exists for application ${app_name} assuming it is not running"
+        echo "pid does not exists for application ${APPLICATION_NAME} assuming it is not running"
+        return 0
     else
-        echo "Getting status of application $app_name, pid $pid"
-        ps -fp $pid
+        echo "Getting status of application $APPLICATION_NAME, pid $pid"
+        ps -fp $pid ; rtrn_cd=$?
+        if [ $rtrn_cd -eq 0 ];then
+            #application running
+            echo "application running"
+            return 1
+        else
+            #application not running
+            echo "application NOT running"
+            return 0
+        fi
     fi
 
 }
