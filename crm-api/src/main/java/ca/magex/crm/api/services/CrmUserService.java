68750071320f2a12cec9a806322b7821bc9f730c
magex9 <scott@magex.ca>
2020-05-13 07:59:48 -0400

    Added default methods for services for helper methods.

--- crm-api/src/main/java/ca/magex/crm/api/services/CrmUserService.java
+++ crm-api/src/main/java/ca/magex/crm/api/services/CrmUserService.java
@@ -5,11 +5,14 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import ca.magex.crm.api.filters.Paging;
 import ca.magex.crm.api.filters.UsersFilter;
 import ca.magex.crm.api.roles.User;
 import ca.magex.crm.api.system.Identifier;
+import ca.magex.crm.api.system.Status;
 
 public interface CrmUserService {
 
@@ -72,4 +75,36 @@ public interface CrmUserService {
 		@NotNull UsersFilter filter, 
 		@NotNull Paging paging
 	);
+	
+	default Page<User> findUsers(@NotNull UsersFilter filter) {
+		return findUsers(filter, defaultUsersPaging());
+	}
+	
+	default Page<User> findActiveUserForOrg(@NotNull Identifier organizationId) {
+		return findUsers(new UsersFilter(organizationId, null, Status.ACTIVE, null, null));
+	}
+	
+	default UsersFilter defaultUsersFilter() {
+		return new UsersFilter();
+	};
+	
+	default Paging defaultUsersPaging() {
+		return new Paging(SORT_OPTIONS.get(0));
+	}
+	
+	public static final List<Sort> SORT_OPTIONS = List.of(
+		Sort.by(Order.asc("username")),
+		Sort.by(Order.desc("username")),
+		Sort.by(Order.asc("personName")),
+		Sort.by(Order.desc("personName")),
+		Sort.by(Order.asc("organizationName")),
+		Sort.by(Order.desc("organizationName")),
+		Sort.by(Order.asc("status")),
+		Sort.by(Order.desc("status"))
+	);
+	
+	default List<Sort> getUsersSortOptions() {
+		return SORT_OPTIONS;
+	}
+	
 }
