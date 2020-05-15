680f35c85e2aef15a35fae53af1331daa9e2ec0d
eagleyes <eagleyes12@gmail.com>
2020-05-15 16:35:45 -0400

    modified service.sh to add comments and restart command

--- crm-admin/admin/service.sh
+++ crm-admin/admin/service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#set -x
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
@@ -54,10 +54,27 @@ if [ ! -d ${work_dir} ];then
     mkdir -p ${work_dir}
 fi
 
+restart(){
+        stop
+        status;rtrn=$?
+        if [ $rtrn != 0 ];then
+                for i in {1..5}
+                do
+                        sleep 10
+                        status;rtrn=$?
+                        if [ $rtrn == 0 ];then
+                                break
+                        fi
+
+                done
+        fi
+        start
+}
 start() {
+    echo "Starting application ${APPLICATION_NAME}"
     #get status
     status ; rtrn_cd=$?
-    if [ $rtrn_cd -eq 1 ];then 
+    if [ $rtrn_cd -eq 1 ];then
         #application running , must stop it first
         echo "application already running"
         exit 0
@@ -71,16 +88,18 @@ start() {
 }
 
 stop() {
-    pid=cat ${pid_file}
-    if [ -z $pid ];then
-        echo "pid does not exists for application ${APPLICATION_NAME} assuming it is not running"
-    else
+    echo "Stopping application ${APPLICATION_NAME}"
+    status ; rtrn_cd=$?
+    if [ $rtrn_cd -eq 1 ];then
+        pid=`cat ${pid_file}`
         echo "killing pid $pid"
         kill -15 $pid
+    else
+        echo "application not running , then nothing to stop"
     fi
 }
 
-#status returns 
+#status returns
 # 0 : not running
 # 1 : running
 status(){
@@ -90,14 +109,14 @@ status(){
         return 0
     else
         echo "Getting status of application $APPLICATION_NAME, pid $pid"
-        ps -fp $pid ; rtrn_cd=$?
+        ps -fp $pid > /dev/null ; rtrn_cd=$?
         if [ $rtrn_cd -eq 0 ];then
             #application running
-            echo "application running"
+            echo "application ${APPLICATION_NAME} running"
             return 1
         else
             #application not running
-            echo "application NOT running"
+            echo "application ${APPLICATION_NAME} NOT running"
             return 0
         fi
     fi
