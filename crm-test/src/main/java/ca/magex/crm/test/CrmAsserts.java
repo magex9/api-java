a9d970469eee43a9ce6606c54f1dea2bef365d51
magex9 <scott@magex.ca>
2020-05-15 22:23:43 -0400

    Added asserts

--- crm-test/src/main/java/ca/magex/crm/test/CrmAsserts.java
+++ crm-test/src/main/java/ca/magex/crm/test/CrmAsserts.java
@@ -12,16 +12,67 @@ import ca.magex.crm.api.common.Telephone;
 import ca.magex.crm.api.exceptions.BadRequestException;
 import ca.magex.crm.api.system.Identifier;
 import ca.magex.crm.api.system.Lang;
+import ca.magex.crm.api.system.Localized;
 
 public class CrmAsserts {
 	
-	public static final MailingAddress ValidCanadianAddress = new MailingAddress("123 Main St", "Ottawa", "Ontario", "Canada", "K1K1K1");
+	public static final Localized GROUP = new Localized("GRP", "Group", "Groupe");
 	
-	public static final PersonName ValidPersonName = new PersonName("Mr.", "Chris", "P", "Bacon");
+	public static final Localized ADMIN = new Localized("ADM", "Admin", "Admin");
 	
-	public static final Communication ValidCommunication = new Communication("Developer", "English", "user@work.ca", new Telephone("5551234567"), null);
+	public static final Localized SYS = new Localized("SYS", "System", "Systeme");
 	
-	public static final BusinessPosition ValidBusinessPosition = new BusinessPosition("Corporate Services", "Development", "Developer");
+	public static final Localized SYS_ADMIN = new Localized("SYS_ADMIN", "System Administrator", "Adminstrator du system");
+	
+	public static final Localized ORG = new Localized("ORG", "Organization", "Organisation");
+	
+	public static final Localized ORG_ADMIN = new Localized("SYS_ADMIN", "Organization Administrator", "Adminstrator du l'organization");
+	
+	public static final Localized ENGLISH = new Localized("EN", "English", "Anglais");
+	
+	public static final Localized FRENCH = new Localized("FR", "French", "Français");
+	
+	public static final Localized CANADA = new Localized("CA", "Canada", "Canada");
+	
+	public static final Localized ALBERTA = new Localized("AB", "Alberta", "Alberta");
+	
+	public static final Localized BRITISH_COLUMBIA = new Localized("BC", "British Columbia", "Colombie britannique");
+	
+	public static final Localized ONTARIO = new Localized("ON", "Ontario", "Ontario");
+	
+	public static final Localized QUEBEC = new Localized("QC", "Quebec", "Québec");
+	
+	public static final Localized UNITED_STATES = new Localized("US", "United States", "États Unis");
+	
+	public static final Localized NEW_YORK = new Localized("NY", "United States", "États Unis");
+	
+	public static final Localized MASSACHUSETTS = new Localized("MA", "Massachusetts", "Massachusetts");
+	
+	public static final Localized ILLINOIS = new Localized("IL", "Illinois", "Illinois");
+	
+	public static final Localized ENGLAND = new Localized("GB", "England", "Angleterre");
+	
+	public static final Localized LONDON = new Localized("JS", "Greater London", "Le Grand Londres");
+	
+	public static final Localized CHESHIRE = new Localized("CH", "Cheshire", "Cheshire");
+	
+	public static final Localized FRANCE = new Localized("FR", "France", "Franche");
+	
+	public static final Localized ILE_DE_FRANCE = new Localized("IF", "Île-de-France", "Île-de-France");
+	
+	public static final Localized MIDI_PYRENESE = new Localized("MD", "Midi-Pyrénées", "Midi-Pyrénées");
+	
+	public static final Localized GERMANY = new Localized("DE", "Germany", "Allemagne");
+	
+	public static final Localized RHINE = new Localized("RH", "North Rhine-Westphalia", "Rhénanie du Nord-Westphalie");
+	
+	public static final MailingAddress MAILING_ADDRESS = new MailingAddress("123 Main St", "Ottawa", QUEBEC, CANADA, "K1K1K1");
+	
+	public static final PersonName PERSON_NAME = new PersonName("Mr.", "Chris", "P", "Bacon");
+	
+	public static final Communication COMMUNICATIONS = new Communication("Developer", ENGLISH, "user@work.ca", new Telephone("5551234567"), null);
+	
+	public static final BusinessPosition BUSINESS_POSITION = new BusinessPosition("Corporate Services", "Development", "Developer");
 	
 	public static <T> void assertSinglePage(Page<T> page, int totalElements) {
 		assertPage(page, totalElements, totalElements, 1, false, false, false, false);
