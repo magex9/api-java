22de8dc02cd26ba2bec1d4e7a32c2eeb690da1cf
Jonny <jonny.trafford@gmail.com>
2020-05-20 12:00:29 -0400

    updated more data fetching

---
+++ crm-graphql-spring-controllers/src/test/java/ca/magex/crm/graphql/datafetcher/LookupDataFetcherTests.java
@@ -0,0 +1,11 @@
+package ca.magex.crm.graphql.datafetcher;
+
+import org.junit.Test;
+
+public class LookupDataFetcherTests extends AbstractDataFetcherTests {
+
+	@Test
+	public void lookupDataFetching() throws Exception {
+		
+	}
+}
