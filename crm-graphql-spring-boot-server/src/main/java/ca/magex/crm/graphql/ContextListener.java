ccab40554462e16c76ec07856640b80f98fef571
Jonny <jonny.trafford@gmail.com>
2020-05-22 13:19:41 -0400

    Update Auth Server Swagger UI
    Now available in any server running with profile
    MagexCrmProfiles.AUTH_EMBEDDED_JWT under the /auth URI

--- crm-graphql-spring-boot-server/src/main/java/ca/magex/crm/graphql/ContextListener.java
+++ crm-graphql-spring-boot-server/src/main/java/ca/magex/crm/graphql/ContextListener.java
@@ -7,32 +7,24 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import ca.magex.crm.api.authentication.CrmPasswordService;
-import ca.magex.crm.api.services.CrmLocationService;
-import ca.magex.crm.api.services.CrmLookupService;
-import ca.magex.crm.api.services.CrmOrganizationService;
-import ca.magex.crm.api.services.CrmPermissionService;
-import ca.magex.crm.api.services.CrmPersonService;
-import ca.magex.crm.api.services.CrmUserService;
-import ca.magex.crm.resource.CrmDataInitializer;
+import ca.magex.crm.api.common.PersonName;
+import ca.magex.crm.api.services.CrmInitializationService;
 
 @Component
 public class ContextListener implements ApplicationListener<ApplicationReadyEvent> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ContextListener.class);
 
-	@Autowired private CrmOrganizationService crmOrganizationService;
-	@Autowired private CrmLocationService crmLocationService;
-	@Autowired private CrmPersonService crmPersonService;
-	@Autowired private CrmUserService crmUserService;
-	@Autowired private CrmLookupService crmLookupService;
-	@Autowired private CrmPermissionService crmPermissionService;
-	@Autowired private CrmPasswordService crmPasswordService;
+	@Autowired private CrmInitializationService initializationService;
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		LOG.info("Initializing Crm Data");
-//		CrmDataInitializer.initialize(crmOrganizationService, crmLocationService, crmPersonService, crmUserService,
-//				crmPermissionService, crmPasswordService, crmLookupService);
+		initializationService.initializeSystem(
+				"Johnnuy", 
+				new PersonName("Mr", "Jonny", "", "Trafford"), 
+				"jonny@johnnuy.org", 
+				"johnnuy", 
+				"admin");
 	}	
 }
