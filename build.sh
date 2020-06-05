ed3e44e6054dde1a0e3d9837ff8c846de888175e
magex9 <scott@magex.ca>
2020-06-04 22:14:58 -0400

    Added a build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+export MAVEN_REPO=x:/api-build
+rm -rf $MAVEN_REPO/logs
+rm -rf $MAVEN_REPO/local-repo/ca/magex
+rm -rf $MAVEN_REPO/deploy-repo/ca/magex
+mkdir -p $MAVEN_REPO/logs
+
+export MAVEN_OPTS="-Dmaven.repo.local=$MAVEN_REPO/local-repo -DaltDeploymentRepository=release-repo::default::file:$MAVEN_REPO/deploy-repo"
+echo MAVEN_OPTS=$MAVEN_OTPS
+
+echo Clean
+mvn clean | tee -a $MAVEN_REPO/logs/clean.log
+
+echo Install
+mvn deploy  | tee -a $MAVEN_REPO/logs/deploy.log
+
+echo Site
+mvn site:deploy | tee -a $MAVEN_REPO/logs/site.log
+
+echo Display a dependency tree
+mvn dependency:tree | tee -a $MAVEN_REPO/logs/dependencies.log
+
+echo Download the findbugs plugin
+mvn findbugs:findbugs | tee -a $MAVEN_REPO/logs/findbugs.log
+
+echo Download the cobertura plugin
+mvn cobertura:cobertura | tee -a $MAVEN_REPO/logs/cobertura.log
+
+echo Download the jacoco plugin
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true | tee -a $MAVEN_REPO/logs/jacoco.log
