362936b7f828df82faff0d7205419cf153a184c4
magex9 <scott@magex.ca>
2020-06-05 10:29:52 -0400

    Added more test entities.

--- crm-test/src/main/java/ca/magex/crm/test/CrmAsserts.java
+++ crm-test/src/main/java/ca/magex/crm/test/CrmAsserts.java
@@ -26,18 +26,14 @@ import ca.magex.crm.api.filters.Paging;
 import ca.magex.crm.api.lookup.BusinessClassification;
 import ca.magex.crm.api.lookup.BusinessSector;
 import ca.magex.crm.api.lookup.BusinessUnit;
+import ca.magex.crm.api.lookup.Salutation;
 import ca.magex.crm.api.system.FilteredPage;
 import ca.magex.crm.api.system.Identifier;
 import ca.magex.crm.api.system.Lang;
 import ca.magex.crm.api.system.Localized;
 import ca.magex.crm.api.system.Message;
 import ca.magex.json.model.JsonArray;
-import ca.magex.json.model.JsonBoolean;
-import ca.magex.json.model.JsonElement;
-import ca.magex.json.model.JsonNumber;
 import ca.magex.json.model.JsonObject;
-import ca.magex.json.model.JsonText;
-import ca.magex.json.util.FormattedStringBuilder;
 
 public class CrmAsserts {
 	
@@ -127,15 +123,43 @@ public class CrmAsserts {
 	
 	public static final String PERSON_FAX = "6138884567";
 	
-	public static final Communication COMMUNICATIONS = new Communication("Developer", ENGLISH.getCode(), "user@work.ca", new Telephone("5551234567", "42"), "8881234567");
+	public static final Communication HOME_COMMUNICATIONS = new Communication(null, ENGLISH.getCode(), "user@home.ca", new Telephone("5558883333", null), null);
 	
-	public static final BusinessSector BUSINESS_SECTOR = new BusinessSector("1", "External", "External");
+	public static final Communication WORK_COMMUNICATIONS = new Communication("Developer", ENGLISH.getCode(), "user@work.ca", new Telephone("5551234567", "42"), "8881234567");
 	
-	public static final BusinessUnit BUSINESS_UNIT = new BusinessUnit("1", "Solutions", "Solutions");
+	public static final BusinessSector BUSINESS_SECTOR = new BusinessSector("1", "Corporate Services", "Corporate Services");
 	
-	public static final BusinessClassification BUSINESS_CLASSIFICATION = new BusinessClassification("1", "Developer", "Développeur");
+	public static final BusinessUnit BUSINESS_UNIT = new BusinessUnit("1", "Communications", "Communications");
+	
+	public static final BusinessClassification BUSINESS_CLASSIFICATION = new BusinessClassification("1", "Translator", "Translator");
 	
 	public static final BusinessPosition BUSINESS_POSITION = new BusinessPosition(BUSINESS_SECTOR.getCode(), BUSINESS_UNIT.getCode(), BUSINESS_CLASSIFICATION.getCode());
+	
+	public static final BusinessSector DEVELOPER_SECTOR = new BusinessSector("2", "External", "External");
+	
+	public static final BusinessUnit DEVELOPER_UNIT = new BusinessUnit("2", "Solutions", "Solutions");
+	
+	public static final BusinessClassification DEVELOPER_CLASSIFICATION = new BusinessClassification("2", "Developer", "Développeur");
+	
+	public static final BusinessPosition DEVELOPER_POSITION = new BusinessPosition(DEVELOPER_SECTOR.getCode(), DEVELOPER_UNIT.getCode(), DEVELOPER_CLASSIFICATION.getCode());
+	
+	public static final Salutation MISS = new Salutation("1", "Miss", "Mlle.");
+
+	public static final Salutation MRS = new Salutation("2", "Mrs.", "Mme.");
+
+	public static final Salutation MR = new Salutation("3", "Mr.", "M.");
+	
+	public static final PersonName ADAM = new PersonName(MR.getCode(), "Adam", "A", "Anderson");
+	
+	public static final PersonName BOB = new PersonName(null, "Bob", "K", "Robert");
+	
+	public static final PersonName CHLOE = new PersonName(null, "Chloé", null, "LaRue");
+	
+	public static final PersonName DAN = new PersonName(MR.getCode(), "Daniel", "D", "O'Sullivan");
+	
+	public static final PersonName ELAINE = new PersonName(MRS.getCode(), "Elaine", "M", "McKay");
+	
+	public static final PersonName FRANCOIS = new PersonName(MR.getCode(), "François", null, "Mátyás");
 	
 	public static final List<Localized> LOCALIZED_SORTING_OPTIONS = List.of(
 		new Localized("A", "A", "A"),
