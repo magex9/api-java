04c0b941431073828bf0d4dce35157c389f95585
johnnuy <Jonny.Trafford@gmail.com>
2020-07-17 12:17:16 -0400

    update documentaiton for type

--- crm-api/src/main/java/ca/magex/crm/api/system/Type.java
+++ crm-api/src/main/java/ca/magex/crm/api/system/Type.java
@@ -34,43 +34,69 @@ import ca.magex.crm.api.system.id.StatusIdentifier;
  */
 public enum Type {
 	
-	// The statuses an entity can be in
+	/**
+	 *  The set of available states for an item within the CRM System 
+	 */
 	STATUS("STATUSES", "Statuses", "Statuts", null, false, false, false, StatusIdentifier.class),
 	
-	// The list of locales the application is available in
+	/**
+	 *  The set of locales available within the CRM system for API calls and data retrieval
+	 */
 	LOCALE("LOCALES", "Locales", "Locaux", null, false, false, false, LocaleIdentifier.class),
 	
-	// The core dictionary of translated phrases
+	/**
+	 *  The core dictionary of translated phrases
+	 */
 	DICTIONARY("DICTIONARIES", "Dictionaries", "Dictionnaires", null, true, true, false, DictionaryIdentifier.class),
 	
-	// The translated phrases of text that can be used in the system from a dictionary
+	/** 
+	 * The translated phrases of text that can be used in the system.  Each phrase is part of a dictionary.
+	 */
 	PHRASE("PHRASES", "Phrases", "Phrases", DICTIONARY, true, true, true, PhraseIdentifier.class),
 	
-	// The type of message presented to the user
+	/**
+	 *  The type of message presented to the user
+	 */
 	MESSAGE_TYPE("MESSAGE_TYPES", "Message Types", "Types de messages", null, false, true, false, MessageTypeIdentifier.class),
 	
-	// The list of authentication groups an organization can belong to
+	/**
+	 *  The list of authentication groups an organization can belong to
+	 */
 	AUTHENTICATION_GROUP("AUTHENTICATION_GROUPS", "Authentication Groups", "Groupes d'authentification", null, true, true, false, AuthenticationGroupIdentifier.class),
 	
-	// The list of authentication roles a user can be given, they must be associated to the authentication group of their organization
+	/**
+	 *  The list of authentication roles a user can be given, they must be associated to the authentication group of their organization
+	 */
 	AUTHENTICATION_ROLE("AUTHENTICATION_ROLES", "Authentication Roles", "Rôles d'authentification", AUTHENTICATION_GROUP, false, true, false, AuthenticationRoleIdentifier.class),
 	
-	// The list of salutations for a person
+	/**
+	 *  The list of salutations for a person
+	 */
 	SALUTATION("SALUTATIONS", "Salutations", "Salutations", null, false, true, true, SalutationIdentifier.class),
 	
-	// The list of languages an entity can be associated with
+	/**
+	 *  The list of languages a person can be associated with for identifiable lookups
+	 */
 	LANGUAGE("LANGUAGES", "Languages", "Langages", null, false, true, true, LanguageIdentifier.class),
 	
-	// The core list of countries
+	/**
+	 *  The list of countries available within the system for identifiable lookups
+	 */
 	COUNTRY("COUNTRIES", "Countries", "Des pays", null, false, true, true, CountryIdentifier.class),
 	
-	// The core list of provinces for their respective countries
+	/**
+	 *  The core list of provinces within the system, each one associated with a Country Identfier
+	 */
 	PROVINCE("PROVINCES", "Canadian Provinces", "Provinces canadiennes", COUNTRY, false, true, true, ProvinceIdentifier.class),
 	
-	// The list of business groups an organization can be assigned
+	/**
+	 *  The list of business groups an organization can be assigned
+	 */
 	BUSINESS_GROUP("BUSINESS_GROUPS", "Business Sector", "Secteur d'activité", null, true, true, false, BusinessGroupIdentifier.class),
 	
-	// The list of business roles a user can be assigned, they must be associated to the business group of an organization
+	/**
+	 *  The list of business roles a user can be assigned, they must be associated to the business group of an organization
+	 */
 	BUSINESS_ROLE("BUSINESS_ROLES", "Business Unit", "Équipe commerciale", BUSINESS_GROUP, false, true, true, BusinessRoleIdentifier.class);
 	
 	private static final Map<String, Type> INDEX = Arrays.asList(values()).stream().collect(Collectors.toMap(t -> t.getCode(), t -> t));
